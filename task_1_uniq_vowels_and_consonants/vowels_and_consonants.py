c58b491f37b988d9a4731d01c1b794d505d6ef6f
Solveyga <azbite@mail.ru>
2024-12-25 11:20:43 +0300

    add simple solution

---
+++ task_1_uniq_vowels_and_consonants/vowels_and_consonants.py
@@ -0,0 +1,21 @@
+'''
+Написать и протестировать функцию, которая:
+- принимает на вход строку
+- возвращает списки кириллических гласных и согласных
+'''
+
+def letters_list(input_string):
+    uniq_letters = set(input_string.lower())
+    vowels = {'а', 'о', 'е', 'ё', 'и', 'у'}
+    consonants = {'б', 'п', 'в', 'ф', 'д', 'т', 'з', 'с', 'ж', 'ш', 'ч', 'ц', 'щ', 'г', 'к', 'х', 'м', 'н', 'л', 'р'}
+    output_vowels = []
+    output_consonants = []
+    for i in uniq_letters:
+        if i in vowels:
+            output_vowels.append(i)
+        elif i in consonants:
+            output_consonants.append(i)
+    return output_vowels, output_consonants
+
+
+print(letters_list('Тестовая строка'))
