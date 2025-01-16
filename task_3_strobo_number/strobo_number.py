168b6a1d42cfb71fbac134df7e89b8299b0e7a77
Solveyga <azbite@mail.ru>
2025-01-16 09:52:14 +0300

    add function annotation and TypeError test

--- task_3_strobo_number/strobo_number.py
+++ task_3_strobo_number/strobo_number.py
@@ -1,20 +1,34 @@
+import unittest
+
+
 def is_strobogram_number(num: str) -> bool:
-    res: str = ''
-    strob_digit = '0689'
-    for i in range(len(num) - 1, -1, -1):
-        if num[i] in strob_digit:
-            if num[i] == '6':
-                res += '9'
-            elif num[i] == '9':
-                res += '6'
+    """
+    Функция принимает строку с целым числом и возвращает True, если число стробограмматическое.
+    Ключевые аргументы:
+    num - строка с целым числом, которую принимает функция
+    reverse_number - num с поворотом на 180 градусов
+    strobogram_digit - стробограмматические цифры
+    """
+    try:
+        reverse_number: str = ''
+        strobogram_digit = '0689'
+        for i in range(len(num) - 1, -1, -1):
+            if num[i] in strobogram_digit:
+                if num[i] == '6':
+                    reverse_number += '9'
+                elif num[i] == '9':
+                    reverse_number += '6'
+                else:
+                    reverse_number += num[i]
             else:
-                res += num[i]
-        else:
-            return False
-    return res == num
+                return False
+        return num == reverse_number
+    except TypeError:
+        return "Ожидается строка с целым числом."
 
 
 """Тест для стробограмматическое число."""
+assert True == is_strobogram_number('86098')
 
 """Тест для числа, у которого все цифры стробограмматические, но само число не подходит под условие."""
 
@@ -24,8 +38,14 @@ def is_strobogram_number(num: str) -> bool:
 
 """Тест проверяет обработку integer вместо string(такая ошибка возникнет при попытке итерации)."""
 
+
+class TestInteger(unittest.TestCase):
+    def test_integer(self):
+        with self.assertRaises(TypeError):
+            is_strobogram_number(609)
+
+
 """
 Вопросы:
 - нужна обработка входных данных? Раз есть условие, что подается строка с числом, я бы сделала только обработку ошибки) 
 """
-
