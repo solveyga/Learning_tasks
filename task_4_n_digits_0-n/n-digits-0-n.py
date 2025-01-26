d5a9aaa0bf80ac6b19c7dbdb9d9f72d17ff4a0ed
Solveyga <azbite@mail.ru>
2025-01-26 19:00:14 +0300

    seconf ineration. Solution through sum and tests caegories.

--- task_4_n_digits_0-n/n-digits-0-n.py
+++ task_4_n_digits_0-n/n-digits-0-n.py
@@ -1,44 +1,41 @@
 import unittest
+import pytest
 
 def missingNumber(nums: list) -> str:
     """
     Функция принимает массив чисел, сортирует его, проверяет пропущенное число в диапазоне [0, n] и возвращает его.
-    Ключевые параметры:
-    expected_num - число со значением, равным индексу числа из массива.
+    :param
+    nums - полученный массив с n числами
+    :returns
+    str - ответ с пропущенным числом
     """
-    try:
-        nums.sort()
-        if nums[-1] > len(nums):
-            raise ValueError("Функция принимает массив из n чисел в диапазоне [0, n]. Передан массив с числом больше n.")
-    except TypeError:
-        return "Функция принимает только массив чисел."
-    except AttributeError:
-        return "Функция принимает только массив чисел."
-    except ValueError as e:
-        return e
-    if nums[-1] != len(nums):
-        return f"Пропущено число {len(nums)}"
-    elif nums[0] != 0:
-        return f"Пропущено число 0"
-    for i in range(1, len(nums)):
-        expected_num = i
-        if nums[i] != expected_num:
-            return f"Пропущено число {expected_num}"
+    n = len(nums)
+    full_sum = n*(n+1) // 2
+    nums_sum = sum(nums)
+    return f"Пропущено число {full_sum - nums_sum}."
 
-"""Тест проверяет обычный массив, в котором не первое и не последнее число."""
-assert "Пропущено число 3" == missingNumber([0, 1, 2, 4])
 
-"""Тест проверяет массив с пропущенным нулем."""
-assert "Пропущено число 0" == missingNumber([3, 1, 2, 4])
+"""
+Позитивные тесты
+"""
+"""Тест проверяет обычный массив, в котором пропущено не первое и не последнее число."""
+assert "Пропущено число 3." == missingNumber([0, 1, 2, 4])
+
+"""Тест проверяет массив с пропущенным нулем. Числа расположены в обратном порядке."""
+assert "Пропущено число 0." == missingNumber([4, 3, 2, 1])
 
 """Тест проверяет массив с пропущенным максимальным числом."""
-assert "Пропущено число 4" == missingNumber([3, 1, 2, 0])
+assert "Пропущено число 4." == missingNumber([3, 1, 2, 0])
 
-"""Тест исключения, в котором массив содержит число больше длины массива."""
-class TestNumberOutside(unittest.TestCase):
-    def test_number_outside(self):
-        with self.assertRaises(ValueError):
-            missingNumber[2]
+"""Тест проверяет минимальный массив, в котором пропущено минимальное число"""
+assert "Пропущено число 0." == missingNumber([1])
+
+"""Тест проверяет минимальный массив, в котором пропущено масимальное число"""
+assert "Пропущено число 1." == missingNumber([0])
+
+"""
+Критичные негативные тесты
+"""
 
 """Тест исключения, в котором передан не массив"""
 class TestNumber(unittest.TestCase):
@@ -50,10 +47,31 @@ class TestNumber(unittest.TestCase):
 class TestNumber(unittest.TestCase):
     def test_number_instead_list(self):
         with self.assertRaises(TypeError):
-            missingNumber(['0', '2'])
+            missingNumber([0, 2])
+
 """
-Вопрос: нужна ли обработка для дробных чисел?
-А для целых отрицательных?
+Остальные негативные тесты
 """
+
+"""Тест необрабатываемого случая, в котором массив содержит число больше своей длины."""
+assert "Пропущено число -1." == missingNumber([2])
+
+
+"""Тест с вещественными числами. Функция не обрабатывает гарантируемый ввод, поэтому результат может быть неожиданным."""
+assert "Пропущено число -0.5." == missingNumber([1.0, 2.5])
+
+"""Тест с дублирующимися данными. Обработка такого не добавлена в функцию."""
+assert "Пропущено число 2." == missingNumber([1, 1, 2])
+
+"""Тест пустого массива. В функции нет обработки входных данных."""
+assert "Пропущено число 0." == missingNumber([])
+
+"""Тест массива со значение None."""
+class TestNone(unittest.TestCase):
+    def test_number_instead_list(self):
+        with self.assertRaises(TypeError):
+            missingNumber([None])
+
+
 
 
