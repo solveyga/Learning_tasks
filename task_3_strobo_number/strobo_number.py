95e1d65b91fca74d370cf49bb1b7d94feaea9348
Solveyga <azbite@mail.ru>
2025-01-26 14:38:11 +0300

    second iteration. Dict and test categories

--- task_3_strobo_number/strobo_number.py
+++ task_3_strobo_number/strobo_number.py
@@ -3,31 +3,36 @@ import unittest
 
 def is_strobogram_number(num: str) -> bool:
     """
-    Функция принимает строку с целым числом и возвращает True, если число стробограмматическое.
-    Ключевые аргументы:
-    num - строка с целым числом, которую принимает функция
-    reverse_number - num с поворотом на 180 градусов
-    strobogram_digit - стробограмматические цифры
+    Функция проверяет, является ли число стробограмматическим, то есть одинаковым при повороте на 180 градусов.
+    :param
+    num: строка с целым числом, которое нужно проверить
+    :return:
+    bool: True, если число стробограмматическое
     """
-    try:
-        reverse_number: str = ""
-        strobogram_digit: str = "018"
-        for i in range(len(num) - 1, -1, -1):
-            if num[i] in strobogram_digit:
-                reverse_number += num[i]
-            elif num[i] == "6":
-                reverse_number += "9"
-            elif num[i] == "9":
-                reverse_number += "6"
-            else:
-                return False
-        return num == reverse_number
-    except TypeError:
-        return "Ожидается строка с целым числом."
+    reverse_digit: list = []
+    strobogram_digit: dict = {"0": "0", "1": "1", "6": "9", "8": "8", "9": "6"}
+    for i in range(
+        len(num) - 1, -1, -1
+    ):  # можно использовать for item in reversed(num), чтобы работать с элементами, а не индексами, но reversed - встроенная функция
+        if num[i] not in strobogram_digit.keys():
+            return False
+        reverse_digit.append(strobogram_digit[num[i]])
+    return num == "".join(reverse_digit)
 
 
-"""Тест для стробограмматическое число."""
+"""
+Позитивные тесты
+"""
+
+"""Тест стробограмматического числа из одной цифры"""
+assert is_strobogram_number("8")
+
+"""Тест для стробограмматического числа с разными цифрами, в том числе заменой цифры."""
 assert is_strobogram_number("1860981")
+
+"""
+Критичные негативные тесты
+"""
 
 """Тест для числа, у которого все цифры стробограмматические, но само число не подходит под условие."""
 assert False == is_strobogram_number("8061")
@@ -35,20 +40,20 @@ assert False == is_strobogram_number("8061")
 """Тест не стробограмматического числа."""
 assert False == is_strobogram_number("9087")
 
+"""
+Остальные негативные тесты
+"""
+
 """Тест для строки, которая не является целым числом."""
 assert False == is_strobogram_number("860.98")
 
-"""Тест проверяет обработку integer вместо string."""
+"""Тест для пустой строки. Вернется True, поскольку нет обработки входных данных, т.к. они гарантированы"""
+assert is_strobogram_number("")
+
+"""Тест для ошибки при передаче числа вместо строки с числом."""
 
 
 class TestInteger(unittest.TestCase):
     def test_integer(self):
         with self.assertRaises(TypeError):
             is_strobogram_number(609)
-
-
-"""
-Вопросы:
-1. Нужна ли обработка входных данных? Раз есть условие, что подается строка с числом, я бы сделала только обработку ошибки) 
-2. В решении литкода 1 считается стробограмматической. Я не нашла определения стробограм. цифр в сети, так что это выглядит как допущение.
-"""
